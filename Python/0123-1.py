672e2621545c9cadeecaad378375e66f2f4e5351
ynjch97 <ynjch2017@naver.com>
2022-01-23 19:14:23 +0900

    파이썬 예제

---
+++ Python/0123-1.py
@@ -0,0 +1,6 @@
+"""
+21.01.23.
+Python IDEL 에디터 사용하기 
+"""
+print("hello world")
+print("by ynjch")
---
+++ Python/0123-2.py
@@ -0,0 +1,13 @@
+"""
+21.02.23
+if 문
+"""
+
+a = 3
+b = 2
+
+if a < b :
+    print("b is bigger than a")
+
+if a > b :
+    print("a is bigger than b")
